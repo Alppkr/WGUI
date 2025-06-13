8a6b1e567a177c18600ebac8a584cbd889de55ee
Alppkr <78530679+Alppkr@users.noreply.github.com>
2025-06-13 23:09:01 +0300

    Fix SQLite migration for password length

--- migrations/versions/3c960201a9c4_increase_hashed_password_length.py
+++ migrations/versions/3c960201a9c4_increase_hashed_password_length.py
@@ -21,18 +21,18 @@ def upgrade():
     insp = sa.inspect(bind)
     cols = [c['name'] for c in insp.get_columns('user')]
     if 'hashed_password' in cols:
-        op.alter_column(
-            'user',
-            'hashed_password',
-            type_=sa.String(length=200),
-            existing_type=sa.String(length=128),
-        )
+        with op.batch_alter_table('user') as batch_op:
+            batch_op.alter_column(
+                'hashed_password',
+                type_=sa.String(length=200),
+                existing_type=sa.String(length=128),
+            )
 
 
 def downgrade():
-    op.alter_column(
-        'user',
-        'hashed_password',
-        type_=sa.String(length=128),
-        existing_type=sa.String(length=200),
-    )
+    with op.batch_alter_table('user') as batch_op:
+        batch_op.alter_column(
+            'hashed_password',
+            type_=sa.String(length=128),
+            existing_type=sa.String(length=200),
+        )
